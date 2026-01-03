22cdaf384ae85f773d61551d711625e18d3831a6
Kerim Sekili <kerimsekilix@gmail.com>
2026-01-03 13:35:03 +0000

    triangle & centered triangle

---
+++ centered_triange.c
@@ -0,0 +1,18 @@
+int main() {
+    int n;
+    
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        for (int space = 0; space < n - i - 1; space++) {
+            printf(" ");
+        }
+        for (int star = 0; star < 2 * i + 1; star++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
